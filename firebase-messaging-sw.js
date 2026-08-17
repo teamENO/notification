89e5a14a34e7aff74ef1e8466f3fa93b23e91493
teamENO <kenzosasaki@icloud.com>
2026-08-17 20:31:00 +0800

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -4,10 +4,10 @@ importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-com
 // ★ KenzoのFirebase設定に書き換える
 firebase.initializeApp({
   apiKey: "AIzaSyA5X-J0s1SAgw8zXn4Y-_ekHao0c67sn-k",
-    authDomain: "send-92385.firebaseapp.com",
-    projectId: "send-92385",
-    messagingSenderId: "912374083524",
-    appId: "1:912374083524:web:a33e61f6f5bc71c207be91"
+  authDomain: "send-92385.firebaseapp.com",
+  projectId: "send-92385",
+  messagingSenderId: "912374083524",
+  appId: "1:912374083524:web:a33e61f6f5bc71c207be91"
   };
 
 });
